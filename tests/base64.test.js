e76440093102257d57d77f123b01b6b178702368
fundhund <marius.mielke@googlemail.com>
2022-02-06 11:20:09 +0100

    Package and tests folder

---
+++ tests/base64.test.js
@@ -0,0 +1,13 @@
+const decryptor = require('@fundhund/decryptor')
+
+test('encrypts base 64', () => {
+    const expected = 'SGVsbG8gd29ybGQ='
+    const actual = decryptor.toBase64('Hello world')
+    expect(actual).toBe(expected)
+})
+
+test('decrypts base 64', () => {
+    const expected = 'Hello world'
+    const actual = decryptor.fromBase64('SGVsbG8gd29ybGQ=')
+    expect(actual).toBe(expected)
+})
