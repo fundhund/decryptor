2f9ff5482c276cab2819e554283ae61018f7a74e
fundhund <marius.mielke@googlemail.com>
2022-02-08 11:46:21 +0100

    Added npm scripts and Prettier attributes

--- src/__tests__/base64.test.ts
+++ src/__tests__/base64.test.ts
@@ -1,13 +1,13 @@
 import * as decryptor from '../index'
 
 test('encrypts base 64', () => {
-    const expected = 'SGVsbG8gd29ybGQ='
-    const actual = decryptor.toBase64('Hello world')
-    expect(actual).toBe(expected)
+  const expected = 'SGVsbG8gd29ybGQ='
+  const actual = decryptor.toBase64('Hello world')
+  expect(actual).toBe(expected)
 })
 
 test('decrypts base 64', () => {
-    const expected = 'Hello world'
-    const actual = decryptor.fromBase64('SGVsbG8gd29ybGQ=')
-    expect(actual).toBe(expected)
+  const expected = 'Hello world'
+  const actual = decryptor.fromBase64('SGVsbG8gd29ybGQ=')
+  expect(actual).toBe(expected)
 })
--- src/__tests__/count.test.ts
+++ src/__tests__/count.test.ts
@@ -1,47 +1,45 @@
 import * as decryptor from '../index'
 
 describe('count', () => {
+  test('counts characters correctly', () => {
+    const expected = { d: 1, e: 1, h: 1, l: 3, o: 2, r: 1, w: 1 }
+    const actual = decryptor.count('Hello world')
+    expect(actual).toStrictEqual(expected)
+  })
 
-    test('counts characters correctly', () => {
-        const expected = {'d': 1, 'e': 1, 'h': 1, 'l': 3, 'o': 2, 'r': 1, 'w': 1}
-        const actual = decryptor.count('Hello world')
-        expect(actual).toStrictEqual(expected)
-    })
+  test('uses regex pattern correctly', () => {
+    const expected = { l: 3 }
+    const actual = decryptor.count('Hello world', { pattern: /l/ })
+    expect(actual).toStrictEqual(expected)
+  })
 
-    test('uses regex pattern correctly', () => {
-        const expected = {'l': 3}
-        const actual = decryptor.count('Hello world', { pattern: /l/ })
-        expect(actual).toStrictEqual(expected)
-    })
+  test('uses caseSensitive correctly', () => {
+    const expected = { A: 3, a: 2 }
+    const actual = decryptor.count('AAaAa', { caseSensitive: true })
+    expect(actual).toStrictEqual(expected)
+  })
 
-    test('uses caseSensitive correctly', () => {
-        const expected = {'A': 3, 'a': 2}
-        const actual = decryptor.count('AAaAa', { caseSensitive: true })
-        expect(actual).toStrictEqual(expected)
-    })
-
-    test('counts umlauts correctly', () => {
-        const expected = {'ä': 2, 'ö': 2, 'ü': 2}
-        const actual = decryptor.count('ÄäÖöÜü')
-        expect(actual).toStrictEqual(expected)
-    })
+  test('counts umlauts correctly', () => {
+    const expected = { ä: 2, ö: 2, ü: 2 }
+    const actual = decryptor.count('ÄäÖöÜü')
+    expect(actual).toStrictEqual(expected)
+  })
 })
 
 describe('isAnagram', () => {
+  test('returns true for anagrams', () => {
+    expect(decryptor.isAnagram('nameless', 'salesmen')).toBe(true)
+  })
 
-    test('returns true for anagrams', () => {
-        expect(decryptor.isAnagram('nameless', 'salesmen')).toBe(true)
-    })
+  test('returns false if no anagrams', () => {
+    expect(decryptor.isAnagram('apple', 'banana')).toBe(false)
+  })
 
-    test('returns false if no anagrams', () => {
-        expect(decryptor.isAnagram('apple', 'banana')).toBe(false)
-    })
+  test('handles uppercase and lowercase correctly', () => {
+    expect(decryptor.isAnagram('nAmElEsS', 'sAlEsMeN')).toBe(true)
+  })
 
-    test('handles uppercase and lowercase correctly', () => {
-        expect(decryptor.isAnagram('nAmElEsS', 'sAlEsMeN')).toBe(true)
-    })
-
-    test('handles umlauts correctly', () => {
-        expect(decryptor.isAnagram('äöü', 'üöä')).toBe(true)
-    })
+  test('handles umlauts correctly', () => {
+    expect(decryptor.isAnagram('äöü', 'üöä')).toBe(true)
+  })
 })
